56eb45c1fbaa4a944e2f0403c15313a79e61ba5e
marcosnocetti <nocettimarcos@gmail.com>
2025-07-08 02:18:48 -0300

    change db var

--- backend/src/config/database.ts
+++ backend/src/config/database.ts
@@ -4,11 +4,11 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const pool = new Pool({
-  host: 'localhost',
-  port: parseInt(process.env.DB_PORT || '5433', 10),
-  user: 'chatbots',
-  password: 'mysecretpassword',
-  database: 'bi_insight_hub',
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT || '5432', 10),
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
 
 export default pool;
